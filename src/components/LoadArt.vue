6b6042399a49d2dee7f7f2e14882684ccc7fc943
cwest9400 <chrstphrwest@gmail.com>
2023-04-22 21:32:38 -0400

    template

---
+++ src/components/LoadArt.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <input type="file" accept="image/*" @change="handleImageChange">
+        <input type="file" accept="image/*" @change="handleFrameChange">
+        <canvas ref="canvas"></canvas>
+        <button @click="exportImage">Export Image</button>
+    </div>
+</template>
