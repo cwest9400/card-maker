b1733a992d333e33e4a0f9af6a1dad4977b5ef0f
cwest9400 <chrstphrwest@gmail.com>
2023-04-22 21:39:08 -0400

    handlechange for image upload

--- src/components/LoadArt.vue
+++ src/components/LoadArt.vue
@@ -6,3 +6,34 @@
         <button @click="exportImage">Export Image</button>
     </div>
 </template>
+
+<script>
+export default{
+    Data() {
+        return {
+            image: null,
+            frame: null,
+            canvas:null,
+        };
+    },
+    mounted() {
+        this.canvas = this.$refs.canvas;
+    },
+    methods: {
+        handleImageChange(e) {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.onload = () => {
+                const img = new Image();
+                img.src = reader.result;
+                img.onload = () => {
+                    this.image = img;
+                };
+            };
+            reader.readAsDataURL(file);
+        },
+        
+    }
+}
+    
+</script>
